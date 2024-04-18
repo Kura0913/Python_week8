fb4e6902a2c1cbed1316473e9c541dae9cf65a91
Daniel Lai <116296953+DWEIEEE@users.noreply.github.com>
2024-04-18 18:38:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -64,13 +64,7 @@ class SocketServer(Thread):
 
 def parser(message,student_dict, address):
     print(f"    server recived:{message} from ({address})")
-    if message['command'] == "add":
-        ServerAddStu(student_dict).execute(message['parameters'])
-    elif message['command'] == "show":
-        ServerShow(student_dict).execute(message['parameters'])
-    else:
-        print("command error")
-        return 0
+    student_dict, reply_msg = Func[message['command']](student_dict).execute(message['parameters'])
     return student_dict, reply_msg
 
 if __name__ == '__main__':
