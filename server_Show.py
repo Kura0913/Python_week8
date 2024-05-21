de9354f56d60e7531ca2c795fcc49d7d83a0a246
Daniel Lai <116296953+DWEIEEE@users.noreply.github.com>
2024-05-21 20:16:23 +0800

    Delete server_Show.py

--- server_Show.py
+++
@@ -1,6 +0,0 @@
-class ServerShow:
-    def __init__(self,student_dict):
-        self.student_dict = student_dict
-
-    def execute(self, parameters):
-            return self.student_dict, {'status' : 'OK','parameters' : self.student_dict}
